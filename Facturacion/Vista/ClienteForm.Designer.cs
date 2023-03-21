704e466b0eedf7016867ed21d78beed3152b52b4
Luis20041009894 <hakudoushy19@hotmail.com>
2023-03-20 22:21:32 -0600

    Update_ClienteForm
    Actualizacion

--- Facturacion/Vista/ClienteForm.Designer.cs
+++ Facturacion/Vista/ClienteForm.Designer.cs
@@ -99,6 +99,7 @@
             // 
             // NuevoButton
             // 
+            this.NuevoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.NuevoButton.Location = new System.Drawing.Point(441, 222);
             this.NuevoButton.Name = "NuevoButton";
             this.NuevoButton.Size = new System.Drawing.Size(84, 49);
